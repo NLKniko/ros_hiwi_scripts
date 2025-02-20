342adb4e8bf75001b37439e1b05395d38ea938ed
NLKniko <nikolanlk@outlook.com>
2025-02-20 13:13:46 +0100

    update the perception script using slam and position

--- perception.py
+++ perception.py
@@ -1,45 +1,37 @@
-import math
+from math import pi
 import time
 from interbotix_xs_modules.locobot import InterbotixLocobotXS
 import rospy
 from geometry_msgs.msg import Twist
 from math import pi
 
-class locobot():
-    def __init__(self):
-        rospy.init_node("locobot_move")
-        rospy.loginfo("Press CTL+C to terminate")
-        self.publisher = rospy.Publisher("locobot/cmd_vel", Twist, queue_size=1)
-        self.msg = Twist()
-        rospy.on_shutdown(self.shutdown)
-        self.rate = rospy.Rate(10)
-        self.run()
-        rospy.spin()
-    
-    def publish(self, msg_type="move"):
-        while self.publisher.get_num_connections()<1:
-            rospy.loginfo("Waiting for connection to publisher...")
-            time.sleep(1)
+# To get started, open a terminal and type...
+# 'roslaunch interbotix_xslocobot_control xslocobot_python.launch robot_model:=locobot_wx250s use_nav:=true use_lidar:=true rtabmap_args:=-d enable_pipeline:=true use_perception:=true'
+# Then change to this directory and type 'python combo_control.py'
 
-        rospy.loginfo("connected")
-        rospy.loginfo("Publishing %s message..." % msg_type)
-        self.publisher.publish(self.msg)
-
-
-    def run(self):
-        self.msg.linear.x = 0.2
-        self.publish()
-        time.sleep(10)
-        rospy.signal_shutdown("We are done here!")
-
-
-    def shutdown(self):
-        self.msg.linear.x = 0
-        self.publish("stop")
 
 def main():
-    bot = InterbotixLocobotXS("locobot_wx250s", arm_model="mobile_wx250s")
+    bot = InterbotixLocobotXS(robot_model="locobot_wx250s", arm_model="mobile_wx250s", use_move_base_action=True)
+    bot.base.move_to_pose(0.5, 0.5, 3.14, True)
     bot.camera.pan_tilt_move(0,0.75)
-    success, clusters = bot.pcl.get_cluster_position(ref_frame="locobot/arm_base_link", sort_axis="y", reverse=True)
+    success, cluster = bot.pcl.get_cluster_position(ref_frame="locobot/arm_base_link", sort_axis="y", reverse=True)
     bot.arm.set_ee_pose_components(x=0.3, z=0.2, moving_time=1.5)
     bot.gripper.open()
+
+    #for cluster in clusters:
+    x, y, z = cluster["position"]
+    bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
+    bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
+    bot.gripper.close()
+    bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
+    bot.arm.go_to_home_pos()
+
+    bot.base.move_to_pose(0, 0, 0, True)
+
+    bot.arm.set_ee_pose_components(x=0.3, z=0.2, moving_time=1.5)
+    bot.arm.set_ee_pose_components(x=x, y=y, z=z-0.05, pitch=0.5)
+    bot.gripper.open()
+    bot.arm.go_to_sleep_pos()
+
+if __name__=='__main__':
+    main()
