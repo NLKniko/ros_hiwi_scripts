b8200a81bb596dbc732e46e3a1db93f351882579
ning <nikolanlk@outlook.com>
2025-02-21 14:50:19 +0100

    go_to_home bug fixed

--- perception.py
+++ perception.py
@@ -12,26 +12,28 @@ from math import pi
 
 def main():
     bot = InterbotixLocobotXS(robot_model="locobot_wx250s", arm_model="mobile_wx250s", use_move_base_action=True)
-    bot.base.move_to_pose(0.5, 0.5, 3.14, True)
+    bot.base.move_to_pose(0.2, -1, 0.5, True)
     bot.camera.pan_tilt_move(0,0.75)
-    success, cluster = bot.pcl.get_cluster_positions(ref_frame="locobot/arm_base_link", sort_axis="y", reverse=True)
+    success, clusters = bot.pcl.get_cluster_positions(ref_frame="locobot/arm_base_link", sort_axis="y", reverse=True)
     bot.arm.set_ee_pose_components(x=0.3, z=0.2, moving_time=1.5)
     bot.gripper.open()
 
     #for cluster in clusters:
-    x, y, z = cluster["position"]
+    for cluster in clusters:
+        x, y, z = cluster["position"]
+        bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
+        bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
+        bot.gripper.close()
+
     bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
-    bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
-    bot.gripper.close()
-    bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
-    bot.arm.go_to_home_pos()
+    bot.arm.go_to_home_pose()
 
     bot.base.move_to_pose(0, 0, 0, True)
 
     bot.arm.set_ee_pose_components(x=0.3, z=0.2, moving_time=1.5)
     bot.arm.set_ee_pose_components(x=x, y=y, z=z-0.05, pitch=0.5)
     bot.gripper.open()
-    bot.arm.go_to_sleep_pos()
+    bot.arm.go_to_sleep_pose()
 
 if __name__=='__main__':
     main()
