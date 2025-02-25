27fc0040857ba65adfeee31add7dda51633e959f
NLKniko <nikolanlk@outlook.com>
2025-02-25 13:46:55 +0100

    add a condition for whether there is something to grab

--- perception.py
+++ perception.py
@@ -19,12 +19,19 @@ def main():
     bot.arm.set_ee_pose_components(x=0.3, z=0.2, moving_time=1.5)
     bot.gripper.open()
 
-    #for cluster in clusters:
-    for cluster in clusters:
-        x, y, z = cluster["position"]
-        bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
-        bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
-        bot.gripper.close()
+    if clusters is not None:
+       for cluster in clusters:
+           x, y, z = cluster["position"]
+           bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
+           bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
+           bot.gripper.close()
+    else:
+        rospy.loginfo("There is nothing here, return to home position")
+        time.sleep(1)
+        bot.camera.pan_tilt_move(0,0.2618)
+        bot.arm.go_to_home_pose()
+        bot.arm.go_to_sleep_pose()
+        bot.base.move_to_pose(0, 0, -1.7, True)
 
     bot.camera.pan_tilt_move(0,0.2618)
 
