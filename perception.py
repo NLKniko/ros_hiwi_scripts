f69a63c05766142dbf6ad011543a943a3271322c
ning <nikolanlk@outlook.com>
2025-03-19 11:31:41 +0100

    change the moving time

--- perception.py
+++ perception.py
@@ -16,7 +16,7 @@ def main():
     bot.base.move_to_pose(0.3, -2, 0.314, True)
     bot.camera.pan_tilt_move(0,0.75)
     success, clusters = bot.pcl.get_cluster_positions(ref_frame="locobot/arm_base_link", sort_axis="y", reverse=True)
-    bot.arm.set_ee_pose_components(x=0.3, z=0.2, moving_time=1.5)
+    bot.arm.set_ee_pose_components(x=0.3, z=0.2, moving_time=2)
     bot.gripper.open()
 
     if len(clusters)>0:
@@ -30,7 +30,7 @@ def main():
            bot.arm.go_to_home_pose()
            bot.arm.go_to_sleep_pose()
            bot.base.move_to_pose(0, 0, -1.7, True)   
-           bot.arm.set_ee_pose_components(x=0.3, z=0.1, moving_time=1.5)
+           bot.arm.set_ee_pose_components(x=0.3, z=0.1, moving_time=2)
            bot.gripper.open()    
            bot.arm.go_to_home_pose()
            bot.arm.go_to_sleep_pose()
@@ -44,7 +44,7 @@ def main():
         bot.arm.go_to_sleep_pose()
         bot.base.move_to_pose(0, 0, -1.7, True)
 
-    bot.arm.set_ee_pose_components(x=0.3, z=0.1, moving_time=1.5)
+    bot.arm.set_ee_pose_components(x=0.3, z=0.1, moving_time=2)
     bot.gripper.open()
     bot.camera.pan_tilt_move(0,0)
     bot.arm.go_to_home_pose()
