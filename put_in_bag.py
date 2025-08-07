b7f40526c194ccda507c352d148a2cce712b0f67
NLKniko <nikolanlk@outlook.com>
2025-08-07 14:52:31 +0200

    fix bug in description

--- put_in_bag.py
+++ put_in_bag.py
@@ -6,7 +6,7 @@ from geometry_msgs.msg import Twist
 
 # To get started, open a terminal and type...
 # 'roslaunch interbotix_xslocobot_control xslocobot_python.launch robot_model:=locobot_wx250s use_nav:=true use_lidar:=true rtabmap_args:=-d enable_pipeline:=true use_perception:=true'
-# Then change to this directory and type 'python combo_control.py'
+# Then change to this directory and type 'python3 put_in_bag.py'
 
 
 def main():
